509ddb982a799f57fbc392a013d7c70a18cc76d3
ameenkhan07 <ameenkhan07@gmail.com>
2016-04-27 02:11:25 +0530

    Updated miniaturized scrape script

--- gitInfo.py
+++ gitInfo.py
@@ -1,59 +1,97 @@
 import urllib2
 import requests
 import json
+import time
+import sys
 
-ACCESS_TOKEN= open('access_token.txt','r').read() #Generated token by github developer site
+# Generated token by github developer site
+ACCESS_TOKEN = open('access_token.txt', 'r').read()
 dir = "DATA/"
+counter = 0
 
-def create_json(req,file):
+
+def create_json(req, data):
     '''
     Generate JSON files, [COMMIT URL , FILES CHANGED]
-    Commit listing API, pagination in outer loop, developer.github.com/v3 
+    Commit listing API, pagination in outer loop, developer.github.com/v3
     '''
-    pop_list = ["files","url"]
-    OUT = dir+str(file)+".json"
-    outfile = open(OUT,"a")
+    global counter
     for commit in req.json():
-        response = requests.get(commit['url']+'?access_token='+ACCESS_TOKEN).json()
-        for key in response.keys():
-            if key not in pop_list:
-                response.pop(key,None)
-        json.dump(response,outfile,indent=2)
+        listy = []
+        if counter < 4900:
+            counter += 1
+            print counter
+
+            response = requests.get(commit['url']+'?access_token='+ACCESS_TOKEN).json()
+
+            if not bool(response):
+                Printer(data)
+
+            # listy.append(response["files"])
+            data.append(response["files"])
+        else:
+            Printer(data, outfile)
     return
+
+def Printer(data, outfile):
+        # outfile.write(str(data) + "\n")
+        json.dump(data,outfile,indent=2)
+        sys.exit(0)
+
 
 def get_repositories():
     '''
     List the top 30 starred Gituhb repositories repositories
     Search API , developer.github.com/v3
     '''
-    list=[]
+    list = []
     API = "https://api.github.com/search/repositories?p=1&q=stars%3A>1&s=stars&type=Repositories"
     req = requests.get(API).json()
+    # print req
     count = 0
     for i in req['items']:
-        count+=1
-        s=''
+        count += 1
+        s = ''
         for x in i['commits_url'].encode('utf-8'):
-            if x ==  '{' :
+            # print x
+            if x == '{':
                 break
             else:
-                s+=str(x)
-
+                s += str(x)
+        # print s
         list.append(s)
     return list
 
 API_LIST = get_repositories()
-file=0
-for API in API_LIST:
-    count=1
-    file+=1
-    API = API + "?access_token="+ACCESS_TOKEN+"&page="
-    while True:#PAGINATION
-        url=API + str(count)
-        print(url)
+file = 1
+# for API in API_LIST:
+API = API_LIST[1]
+OUT = dir+str(file)+".json"
+outfile = open(OUT, "w")
+count = 1 #Pagination
+data = []
+flag = 0
+API = API + "?access_token="+ACCESS_TOKEN+"&page="
+while True:  # PAGINATION
+    url = API + str(count)
+    print(url)
+    if flag == 1:
+        break
+    if counter < 4900:
+        counter += 1
         req = requests.get(url)
-        if not bool(req.json()):
-            break
-        print req.status_code #Testing
-        create_json(req,file)
+
+        if not(bool(req.json())):  # All commits listed
+            Printer(data, outfile)
+
+        print req.status_code  # Testing
+        create_json(req, data)
+        print "COUNT" + str(count)
+        # pickle.dump(data, outfile)
         count += 1
+    else:
+            Printer(data, outfile)
+
+        # time.sleep(4000)
+        # print counter
+        # counter = 0
