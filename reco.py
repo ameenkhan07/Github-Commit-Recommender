33674604f6b46f892e9f70c37da69c5e50bb2610
ameenkhan07 <ameenkhan07@gmail.com>
2016-05-03 13:13:12 +0530

    rm Backen

--- reco.py
+++ reco.py
@@ -1,3 +1,7 @@
+#
+# Author Sharan Agrawal
+#
+
 import json
 import sys
 
@@ -5,39 +9,37 @@ import sys
 i = 0
 reco = ""
 rule_split = []
-commit = []
+# commit = []
 
-commit = sys.argv
+# commit = sys.argv
 
-with open(commit[1]) as f:
-	     data = json.load(f)
+with open("COMMIT_SAMPLE.json") as f:
+    data = json.load(f)
 
 f.close()
 
 # print(len(data[0]))
 
 while(i < len(data[0])):
-	
-	file = data[0][i]['filename']
-	# print(file)
-	searchfile = open("rules.txt", "r")
 
-	for line in searchfile:
+    file = data[0][i]['filename']
+    # print(file)
+    searchfile = open("rules.txt", "r")
 
-		rule_split = line.split('==>',1)
-		# print(rule_split[0])
-		# print(file)
-		
-		if (rule_split[0].find(file)) != -1:
-			
-			# print(rule_split[0])
-			reco += line	
+    for line in searchfile:
 
+        rule_split = line.split('==>', 1)
+        # print(rule_split[0])
+        # print(file)
 
-	searchfile.close()
-	i = i + 1
-	# print(i)   	
+        if (rule_split[0].find(file)) != -1:
+
+            # print(rule_split[0])
+            reco += line
+
+    searchfile.close()
+    i = i + 1
+    # print(i)
 
 
 print(reco)
-
